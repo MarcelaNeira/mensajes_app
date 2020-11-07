1f47b4c5461fe921b57be840c0939945310f4ab9
Marcela Neira <martaymarcela@gmail.com>
2020-11-07 11:38:32 +0100

    Primer commit

---
+++ src/main/java/com/mycompany/mensajes_app/Conexion.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mensajes_app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author marta
+ */
+public class Conexion {
+    public Connection getConnection(){
+        Connection conection =null;
+        
+        try{
+           conection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mensajes_app","root","") ;
+           
+           if(conection!=null){
+               System.out.print("Conexion exitosa");
+           }
+        }catch(SQLException e){
+            System.out.print(e);
+        }
+        return conection;
+    }
+}
